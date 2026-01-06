7bc45e418e501faa24d4c96b17f854bece6300bf
Dennis Buskes <d.buskes@gmail.com>
2026-01-06 10:58:45 +0100

    Remove git script

--- git.ps1
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Initial release - PSM v1.0"
-git branch -M main
-git remote add origin https://github.com/dbuskes/proxmox-storage-management.git
-git push -u origin main
